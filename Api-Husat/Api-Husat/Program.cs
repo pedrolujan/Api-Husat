f9abef8dfb3dd75d705b8a601790341734fee046
DESKTOP-ROLGOA1\HP <lujanmarcelo1203@gmail.com>
2023-06-03 18:09:29 -0500

    dando permisos

--- Api-Husat/Api-Husat/Program.cs
+++ Api-Husat/Api-Husat/Program.cs
@@ -8,6 +8,10 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseCors(builder => builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
